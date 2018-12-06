6d15772ab3c1a5878f34f69b1877dbd210c9e28b
Can <cantoron@gmail.com>
2018-12-06 19:14:52 +0300

    Initial redmine yh theme

---
+++ README.md
@@ -0,0 +1,25 @@
+# yh redmine theme #
+Modern Redmine Theme.
+
+## Installation
+
+* Download and unzip latest release and copy `yh` folder into your Redmine themes directory (e.g. ` ../public/themes/`)
+* Restart Redmine to make the newly installed theme available in your theme list.
+* Go to `"Administration / Settings / Display"` and choose this theme (yh) in themes list.
+* Save your changes.
+
+## Contributing
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
+
+## Screenshots
+![](https://raw.githubusercontent.com/yenihayat/redmine-theme-yh/master/screenshots/activity2.png)
+___
+![](https://raw.githubusercontent.com/yenihayat/redmine-theme-yh/master/screenshots/issue2.png)
+___
+![](https://raw.githubusercontent.com/yenihayat/redmine-theme-yh/master/screenshots/issues.png)
+___
+![](https://raw.githubusercontent.com/yenihayat/redmine-theme-yh/master/screenshots/admin.png)
---
+++ yh/javascripts/theme.js
@@ -0,0 +1,49 @@
+
+function createBtn() {
+	$("input[type=submit]").addClass("btn btn-sm btn-primary");
+	$(".tab-content p a.icon").addClass("btn btn-sm btn-primary");
+	$("#login-submit").addClass("btn btn-sm btn-primary");
+	$("input[type=submit] + a.icon").addClass("btn btn-sm btn-outline-dark");
+	$(".controller-enumerations.action-index #content p > a.icon").addClass("btn btn-sm btn-primary");
+
+	$("#content").children(".contextual").find("a:first").addClass("btn btn-sm btn-primary");
+	$("#content").children(".contextual").find("a:not(:first):not(.lock)").addClass("btn btn-sm btn-outline-dark");
+	
+	$(".issue .description .contextual a").addClass("btn btn-sm btn-outline-dark");
+	$(".issue #relations .contextual a, .issue #issue_tree .contextual a").addClass("btn btn-sm btn-outline-dark");
+
+	$("#content .contextual a.lock").addClass("btn btn-sm btn-danger");
+	$(".news.box a:last").addClass("btn btn-sm btn-outline-dark");
+	$("p.buttons a").addClass("btn btn-sm btn-outline-dark");
+	$(".icon-fav-off").addClass("btn btn-sm btn-outline-dark");
+	$("#activity_scope_form").find("p input").addClass("btn btn-sm btn-dark");
+	$("table.query-columns .buttons input[type=button]").addClass("btn btn-sm btn-outline-dark");
+}
+
+function createDOMObserver(targetNode, callback) {
+	if (targetNode) {
+		const config = { attributes: true, childList: true, subtree: true };
+		const observer = new MutationObserver(callback);
+		observer.observe(targetNode, config);	
+	}
+}
+
+function init() {
+	$("table").wrap("<div class='table'></div>");
+	createBtn();
+}
+
+
+$(document).ready(function() {
+	init();
+
+	createDOMObserver(document.querySelectorAll('body')[0], (mutationsList, observer) => {
+		for(var mutation of mutationsList) {
+        if (mutation.type == 'attributes') {
+          $("#member-add-submit").addClass("btn btn-sm btn-primary");
+	 				$("#member-add-submit + input").addClass("btn btn-sm btn-outline-dark");
+        }
+    }
+	});
+
+});
